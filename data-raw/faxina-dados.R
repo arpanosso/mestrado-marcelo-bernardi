e6ad96f31121a8ea290ed593fdfd1000fff62a85
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2025-11-14 10:35:56 -0300

    anova - add

--- data-raw/faxina-dados.R
+++ data-raw/faxina-dados.R
@@ -13,27 +13,32 @@ dados <- readxl::read_xlsx("data-raw/Estatistica_Extratos_Alan_v1.xlsx") |>
   select(tratamento:repeticao,icm_1,icm_2) |>
   mutate(
     trat = case_when(
-      tratamento == 1  ~ "Álcool",
+      tratamento == 1  ~ "Alcool",
       tratamento == 2  ~ "Lavanda",
       tratamento == 3  ~ "Canela",
       tratamento == 4  ~ "Pimenta",
       tratamento == 5  ~ "Alho",
-      tratamento == 6  ~ "Limão",
-      tratamento == 7  ~ "Açafrão",
-      tratamento == 8  ~ "Ora-pro-nóbis",
+      tratamento == 6  ~ "Limao",
+      tratamento == 7  ~ "Acafrao",
+      tratamento == 8  ~ "Ora-pro-nobis",
       tratamento == 9  ~ "Eucalipto",
-      tratamento == 10 ~ "Orégano",
-      tratamento == 11 ~ "Hortelã",
+      tratamento == 10 ~ "Oregano",
+      tratamento == 11 ~ "Hortela",
       tratamento == 12 ~ "Cravo",
       tratamento == 13 ~ "Arruda",
       tratamento == 14 ~ "Boldo",
       tratamento == 15 ~ "Gengibre",
-      tratamento == 16 ~ "Manjericão",
+      tratamento == 16 ~ "Manjericao",
       tratamento == 17 ~ "Tomilho",
       tratamento == 18 ~ "Testemunha",
       TRUE ~ NA_character_
     )
+  ) |>
+  mutate(
+    icm_1 = ifelse(icm_1 <0,0,icm_1),
+    icm_2 = ifelse(icm_2 <0,0,icm_2)
   )
+
 glimpse(dados)
 
 write_rds(dados,
